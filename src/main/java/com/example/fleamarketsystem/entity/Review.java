7b5d93ab89c56584c3da8732de7c11a7533cfe9a
河盛晃久 <kawamoriakihisa@ip-10-95-1-64.ap-northeast-1.compute.internal>
2025-12-08 17:01:53 +0900

    Review.java/User.java の追加

--- src/main/java/com/example/fleamarketsystem/entity/Review.java
+++ src/main/java/com/example/fleamarketsystem/entity/Review.java
@@ -1,5 +1,42 @@
 package com.example.fleamarketsystem.entity;
 
+import jakarta.persistence.*;
+
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "review")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Review {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne
+	@JoinColumn(name = "reviewer_id", nullable = false)
+	private User reviewer;
+
+	@ManyToOne
+	@JoinColumn(name ="seller_id", nullable = false)
+	private User seller;
+
+	@ManyToOne
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
+
+	@Column(nullable = false)
+	private Integer rating;
+
+	@Column(columnDefinition = "TEXT")
+	private String commit;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
 }
