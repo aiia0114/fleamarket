b1ee9761f257acc0b9b98d17afd313be157378f5
河盛晃久 <kawamoriakihisa@ip-10-95-1-64.ap-northeast-1.compute.internal>
2025-12-08 15:17:05 +0900

    FavoriteItem.java の追加

--- src/main/java/com/example/fleamarketsystem/entity/FavoriteItem.java
+++ src/main/java/com/example/fleamarketsystem/entity/FavoriteItem.java
@@ -1,5 +1,32 @@
 package com.example.fleamarketsystem.entity;
 
+import jakarta.persistence.*;
+
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "favorite_item")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FavoriteItem {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
 }
