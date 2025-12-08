dd53b0f2c6272c754313a27b7577b2cf700a4db0
河盛晃久 <kawamoriakihisa@ip-10-95-1-64.ap-northeast-1.compute.internal>
2025-12-08 16:06:00 +0900

    item.java の追加

--- src/main/java/com/example/fleamarketsystem/entity/Item.java
+++ src/main/java/com/example/fleamarketsystem/entity/Item.java
@@ -1,5 +1,46 @@
 package com.example.fleamarketsystem.entity;
 
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="item")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", unique = false)
+	private User seller;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	@Column(nullable = false)
+	private BigDecimal price;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category vategory;
+
+	private String status = "出品中";
+
+	private String imageUrl;
+
+	@Column(name = "created_at", unique = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
 }
